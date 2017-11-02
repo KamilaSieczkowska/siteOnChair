32a69573dca7a747fab341d9e7c50d1b5589172f
Kamila Sieczkowska <kamisia@student.edu>
2017-11-02 10:00:07 +0100

    with slider (desktop version)

--- js/scripts.js
+++ js/scripts.js
@@ -59,7 +59,57 @@ document.addEventListener('DOMContentLoaded', function() {
     
     //------------- slider ----------------------
 
-    
+    var slider = document.querySelector('#main-slider');
+    var prev = slider.querySelector(':scope .left');
+    var next = slider.querySelector(':scope .right');
+    var slides = slider.querySelectorAll(':scope .slide');
+    var currentSlide = 0; 
+    var timer = null; 
+    var timeDelay = 5000;
+
+    var timeoutNextSlide = function() {
+        timer = setTimeout(function() {
+            next.click();
+        }, timeDelay);
+    };
+
+    var prevSlide = function() {
+        for (var i=0; i<slides.length; i++) {
+            slides[i].classList.remove('active');
+        }
+        currentSlide--;
+
+        if (currentSlide < 0) {
+            currentSlide = slides.length-1;
+        }
+
+        slides[currentSlide].classList.add('active');
+
+        clearTimeout(timer);
+        timeoutNextSlide();
+    };
+
+    var nextSlide = function() {
+        for (var i=0; i<slides.length; i++) {
+            slides[i].classList.remove('active');
+        }
+
+        currentSlide++;
+
+        if (currentSlide > slides.length-1) {
+            currentSlide = 0;
+        }
+       
+        slides[currentSlide].classList.add('active');
+
+        clearTimeout(timer);
+        timeoutNextSlide();
+    };
+
+    timeoutNextSlide();
+
+    prev.addEventListener('click', prevSlide);
+    next.addEventListener('click', nextSlide);
 
     //-------------pictures --------------
 
